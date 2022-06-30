8573e40de9a97e4470f9bd54df02c5b2e6c366b5
Matthew Mullen <matthew.mullenx@gmail.com>
2022-06-30 18:52:02 +0700

    work

--- bear-memory-api.go
+++ bear-memory-api.go
@@ -41,8 +41,9 @@ func postBearMemory(c *gin.Context) {
 		fmt.Println("IS IT HERE")
 		log.Fatalln(err)
 	}
-	res, er := db.Exec("INSERT INTO BearMemories (creationDate, base64StringOfFile) VALUES (" + string(rune(newBearMemory.CreationDate)) + ", " + newBearMemory.Base64StringOfFile + ";")
+	res, er := db.Exec("INSERT INTO BearMemories (creationDate, base64StringOfFile) VALUES (" + fmt.Sprint(newBearMemory.CreationDate) + ", " + newBearMemory.Base64StringOfFile + ";")
 	if er != nil {
+		fmt.Println("BEAR BEHAVIOR")
 		log.Fatalln(er)
 	}
 	fmt.Println(res.LastInsertId())
